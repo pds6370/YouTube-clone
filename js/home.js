904effeb590d7606e05d1e0509784216c5ed8ee6
pds6370 <116141143+pds6370@users.noreply.github.com>
2023-01-19 22:53:59 +0900

    add

--- js/home.js
+++ js/home.js
@@ -12,14 +12,32 @@ const briefAndmore2 = document.querySelector(".viewmore2 .briefAndmore");
 const rotate1 = document.querySelector(".viewmore1 .style-scope");
 const rotate2 = document.querySelector(".viewmore2 .style-scope");
 const sideHidden = "hidden1";
-
+const section = document.querySelector(".section");
+// 1000px 이하이면 true 아니면 false
+const mediaMin = window.matchMedia("screen and (max-width: 1000px)");
 // 3선 클릭 시 사이드 바
-function hiddenAside1() {
-  side.classList.toggle("hiddenaside");
-  side.style.transition = "transform 0.2s linear";
+window.addEventListener("load", hiddenAsideWidth);
+window.addEventListener("resize", hiddenAsideWidth);
+
+// 1000px이하 trasition 부여
+function hiddenAsideWidth() {
+  console.log(mediaMin.matches);
+  if (mediaMin.matches) {
+    icon1.addEventListener("click", hiddenAside1);
+    icon2.addEventListener("click", hiddenAside1);
+    function hiddenAside1() {
+      side.classList.toggle("hiddenaside");
+      side.style.transition = "transform 0.2s linear";
+    }
+  } else {
+    icon1.addEventListener("click", hiddenAside2);
+    icon2.addEventListener("click", hiddenAside2);
+    function hiddenAside2() {
+      side.classList.toggle("hiddenaside");
+      side.style.transition = "none";
+    }
+  }
 }
-icon1.addEventListener("click", hiddenAside1);
-icon2.addEventListener("click", hiddenAside1);
 
 // 사이드바 더 보기
 //2개 같은 class
@@ -52,19 +70,29 @@ viewmore2.addEventListener("click", () => {
 briefAndmore2.innerText = "더보기";
 
 //홈 사이드 미디어쿼리
-const section = document.querySelector(".section");
-// 1000px 이하이면 true 아니면 false
-const mediaMin = window.matchMedia("screen and (max-width: 1000px)");
 
-mediaMin.addEventListener("change", () => {
-  // console.log(mediaMin);
-  // console.dir(side.style);
-  side.style.transition = "none";
-  if (mediaMin.matches) {
+mediaMin.addEventListener("change", mediaQuery);
+window.addEventListener("load", mediaQuery);
+// console.log(mediaMin);
+// console.dir(side.style);
+function mediaQuery() {
+  //참*참
+  if (side.classList.contains("hiddenaside") && mediaMin.matches == true) {
     side.classList.add("hiddenaside");
-    icon1.addEventListener("click", hiddenAside1);
-    icon2.addEventListener("click", hiddenAside1);
-  } else {
+    // side.style.transition = "transform 0.2s linear";
+    //참*거짓
+  } else if (
+    side.classList.contains("hiddenaside") &&
+    mediaMin.matches == false
+  ) {
     side.classList.remove("hiddenaside");
+    side.style.transition = "none";
+    //
+  } else if (
+    !side.classList.contains("hiddenaside") &&
+    mediaMin.matches == true
+  ) {
+    side.classList.add("hiddenaside");
   }
-});
+  side.style.transition = "none";
+}
